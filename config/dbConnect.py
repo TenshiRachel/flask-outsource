6fa7499ae657d9b925af78f948e7f5ccff0ca28a
Anthony <joshdebean@gmail.com>
2021-04-02 22:54:37 +0800

    rebuild with service table

--- config/dbConnect.py
+++ config/dbConnect.py
@@ -1,8 +1,9 @@
 from config.dbConfig import Base
 from models.user import User
+from models.service import Service
 
 
 def setupdb():
     Base._meta.db.connect()
-    Base._meta.db.create_tables([User])
+    Base._meta.db.create_tables([User, Service])
     Base._meta.db.close()
