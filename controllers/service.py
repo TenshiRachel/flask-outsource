46672bb920f7409cbeb7d84b3720b1881470df41
Anthony <joshdebean@gmail.com>
2021-04-06 01:37:56 +0800

    create service

--- controllers/service.py
+++ controllers/service.py
@@ -1,7 +1,9 @@
 import config.constants
 from flask import Blueprint, render_template, request, url_for, redirect, flash, session
+from datetime import date
 from middlewares.auth import is_auth
 from models.service import Service
+from models.user import User
 
 
 service_bp = Blueprint('service', __name__, template_folder=config.constants.template_dir,
@@ -18,6 +20,11 @@ def list_service():
 @is_auth
 def manage():
     user_id = session['user_id']
+
+    if User.get(User.id == user_id).acc_type == 'client':
+        flash('You need a service provider account to manage services', 'error')
+        redirect(url_for('index.index'))
+
     services = Service.select().where(Service.uid == user_id)
     return render_template('service/manage.html', services=services)
 
@@ -25,14 +32,25 @@ def manage():
 @service_bp.route('/add', methods=['GET', 'POST'])
 @is_auth
 def add():
+    user_id = session['user_id']
+
+    if User.get(User.id == user_id).acc_type == 'client':
+        flash('You need a service provider account to add service', 'error')
+        redirect(url_for('index.index'))
+
     if request.method == 'POST':
         req = request.form
 
         name = req.get('name')
         desc = req.get('desc')
         price = req.get('price')
-        categories = req.get('categories')
+        categories = req.getlist('categories')
 
-        pass
+        Service.create(name=name, desc=desc, price=price, categories=categories,
+                       date_created=date.today().strftime('%d/%m/%Y'),
+                       views=0, favs=0, uid=user_id)
+
+        flash('Service created successfully', 'success')
+        redirect(url_for('service.manage'))
 
     return render_template('service/add.html')
--- public/js/service.js
+++ public/js/service.js
@@ -1,3 +1,100 @@
+//List service
+function search() {
+    let input = $('#search').val();
+    let filter = input.toUpperCase();
+    let cards = $('.servicecards');
+    let name = $('.name');
+
+    for (let i = 0, n = cards.length; i < n; i++) {
+        let focus = $(cards[i]);
+        let compare = $(name[i]).data('names');
+
+        if (compare.toUpperCase().includes(filter)) {
+            if ($(focus).hasClass('d-none')) {
+                $(focus).removeClass('d-none');
+                $(focus).addClass('animated faster fadeIn').one(animationEnd, function() {
+                    let _this = $(this);
+
+                    _this.removeClass('animated faster fadeIn');
+                    _this.addClass('d-flex');
+                });
+            }
+        }
+        else {
+            if (!$(focus).hasClass('d-none')){
+                $(focus).addClass('animated faster fadeOut').one(animationEnd, function(){
+                    let _this = $(this);
+
+                    _this.removeClass('animated faster fadeOut');
+                    _this.removeClass('d-flex');
+                    _this.addClass('d-none');
+                })
+            }
+        }
+    }
+}
+
+$(function() {
+    let searchbut = document.getElementById('searchbut');
+
+    if (searchbut) {
+        searchbut.addEventListener('click', search);
+    }
+});
+
+$('select.category-select').on('change', function(e) {
+    let cards = $('.servicecards');
+    let category = $(this).find('option:selected').text().replace(/\s/g, '');
+
+    for (let i = 0, n = cards.length; i < n; i++) {
+        let focus = $(cards[i]);
+
+        if (($(focus).hasClass(category) || category === "ShowAll")) {
+            if ($(focus).hasClass('d-none')) {
+                $(focus).removeClass('d-none');
+                $(focus).addClass('animated faster fadeIn').one(animationEnd, function() {
+                    let _this = $(this);
+
+                    _this.removeClass('animated faster fadeIn');
+                    _this.addClass('d-flex');
+                });
+            }
+        }
+        else {
+            if (!$(focus).hasClass('d-none')){
+                $(focus).addClass('animated faster fadeOut').one(animationEnd, function(){
+                    let _this = $(this);
+
+                    _this.removeClass('animated faster fadeOut');
+                    _this.addClass('d-none');
+                    _this.removeClass('d-flex');
+                })
+            }
+        }
+    }
+});
+
+$('select.sort-select').on('change', function(e) {
+    let cards = $('.servicecards');
+    let sort = $(this).find('option:selected').text();
+    if (sort == "Most viewed"){
+        cards.sort(function(a, b){ return $(b).data("views")-$(a).data("views")});
+        $("#servcon").html(cards);
+    }
+    else if (sort == "Newest first"){
+        cards.sort(function(a, b){ return $(b).data("date")-$(a).data("date")});
+        $("#servcon").html(cards);
+    }
+    else if (sort == "Oldest first"){
+        cards.sort(function(a, b){ return $(a).data("date")-$(b).data("date")});
+        $("#servcon").html(cards);
+    }
+    else if (sort == "Most Popular"){
+        cards.sort(function(a, b){ return $(b).data("favs")-$(a).data("favs")});
+        $("#servcon").html(cards);
+    }
+});
+
 function categoryCheck() {
     var categories = document.getElementsByName('categories');
     var error = document.getElementById('categoryErr');
