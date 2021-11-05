d335ed3ba129c0590396ab3cbd57f2f48f1ee8b7
Joshua Goh <joshdebean@gmail.com>
2021-11-05 15:58:04 +0800

    rename logic

--- controllers/files.py
+++ controllers/files.py
@@ -1,6 +1,7 @@
 import config.constants
 import magic
 import os
+import re
 from flask import Blueprint, render_template, request, url_for, redirect, flash, session
 from middlewares.auth import is_auth
 from models.user import User
@@ -52,16 +53,22 @@ def share():
     req = request.form
     fid = req.get('fid')
     share_user = req.get('shareUser')
-    file = File.get_by_id(fid)
-    share_user = User.select().where((User.username == share_user) | (User.email == share_user))
-    share_username = share_user[0].username
-    share_list = file.shareUid.split(',').append(share_user[0].id)
-    share_user = ",".join(share_list)
 
-    query = File.update(shareUid=share_user).where(File.id == fid)
-    query.execute()
+    if fid:
+        file = File.get_by_id(fid)
+        share_user = User.select().where((User.username == share_user) | (User.email == share_user))
+        share_username = share_user[0].username
+        share_list = file.shareUid.split(',').append(share_user[0].id)
+        share_user = ",".join(share_list)
 
-    flash('Successfully shared with ' + share_username, 'success')
+        query = File.update(shareUid=share_user).where(File.id == fid)
+        query.execute()
+
+        flash('Successfully shared with ' + share_username, 'success')
+
+    else:
+        flash('No file or folder selected', 'error')
+
     return redirect(url_for('files.manage'))
 
 
@@ -71,16 +78,44 @@ def unshare():
     req = request.form
     fid = req.get('fid')
     unshare_user = req.getlist('shareUser')
-    file = File.get_by_id(fid)
-    share_list = file.shareUid.split(',')
 
-    for user_id in unshare_user:
-        share_list.remove(user_id)
+    if fid:
+        file = File.get_by_id(fid)
+        share_list = file.shareUid.split(',')
 
-    share_list = ",".join(share_list)
+        for user_id in unshare_user:
+            share_list.remove(user_id)
 
-    query = File.update(shareUid=share_list).where(File.id == fid)
-    query.execute()
+        share_list = ",".join(share_list)
 
-    flash('Unshared successfully', 'success')
+        query = File.update(shareUid=share_list).where(File.id == fid)
+        query.execute()
+
+        flash('Unshared successfully', 'success')
+
+    else:
+        flash('No file or folder selected', 'error')
+
+    return redirect(url_for('files.manage'))
+
+
+@files_bp.route('/rename', methods=['POST'])
+@is_auth
+def rename():
+    req = request.form
+    fid = req.get('fid')
+    name = req.get('rename')
+    regex = re.compile(r'/[!@#$%^&*+\=\[\]{}()~;:"\\|,.<>\/?]/')
+    if fid:
+        if re.match(regex, name):
+            flash('File or folder name only allow alphanumeric, space, underscore and dash', 'error')
+
+        query = File.update(name=name).where(File.id == fid)
+        query.execute()
+
+        flash('File or folder renamed successfully', 'success')
+
+    else:
+        flash('No file or folder selected', 'error')
+
     return redirect(url_for('files.manage'))
