ae752f90518fb2552194edf1f63eef28cf1a5724
Joshua Goh <joshdebean@gmail.com>
2022-04-15 16:36:25 +0800

    fix notifications

--- controllers/notifications.py
+++ controllers/notifications.py
@@ -15,31 +15,31 @@ def index():
     user_id = session.get('user_id')
     user = User.get_by_id(user_id)
 
-    like_notifications = Notification.select().where(Notification.user == user_id & Notification.category == 'likes')\
+    like_notifications = Notification.select().where((Notification.user == user_id) & (Notification.category == 'likes'))\
         .order_by(Notification.date.desc())
 
     job_reject_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'job_reject').order_by(Notification.date.desc())
+        where((Notification.user == user_id) & (Notification.category == 'job_reject')).order_by(Notification.date.desc())
 
     job_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'job').order_by(Notification.date.desc())
+        where((Notification.user == user_id) & (Notification.category == 'job')).order_by(Notification.date.desc())
 
     paid_jobs_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'job_paid').order_by(Notification.date.desc())
+        where((Notification.user == user_id) & (Notification.category == 'job_paid')).order_by(Notification.date.desc())
 
     request_cancelled_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'request_cancel')\
+        where((Notification.user == user_id) & (Notification.category == 'request_cancel'))\
         .order_by(Notification.date.desc())
 
     complete_requests_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'request_complete')\
+        where((Notification.user == user_id) & (Notification.category == 'request_complete'))\
         .order_by(Notification.date.desc())
 
     file_share_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'file_share').order_by(Notification.date.desc())
+        where((Notification.user == user_id) & (Notification.category == 'file_share')).order_by(Notification.date.desc())
 
     file_unshare_notifications = Notification.select().\
-        where(Notification.user == user_id & Notification.category == 'file_unshare').order_by(Notification.date.desc())
+        where((Notification.user == user_id) & (Notification.category == 'file_unshare')).order_by(Notification.date.desc())
 
     return render_template('notifications/index.html', user=user, like_notifications=like_notifications,
                            job_reject_notifications=job_reject_notifications, job_notifications=job_notifications,
@@ -48,3 +48,34 @@ def index():
                            file_unshare_notifications=file_unshare_notifications,
                            complete_requests_notifications=complete_requests_notifications,
                            paid_jobs_notifications=paid_jobs_notifications)
+
+
+@notifications_bp.route('/delete/<id>')
+@is_auth
+def delete(id):
+    if Notification.get_or_none(Notification.id == id) is None:
+        flash('Notification could not be found', 'error')
+        return redirect(url_for('notifications.index'))
+
+    query = Notification.delete().where(Notification.id == id)
+    query.execute()
+
+    flash('Notification deleted successfully', 'success')
+    return redirect(url_for('notifications.index'))
+
+
+@notifications_bp.route('/deleteAll/<category>')
+@is_auth
+def delete_all(category):
+    user_id = session['user_id']
+    count = len(Notification.select().where((Notification.user == user_id) & (Notification.category == category)))
+
+    if count <= 0:
+        flash('Notifications could not be found', 'error')
+        return redirect(url_for('notifications.index'))
+
+    query = Notification.delete().where((Notification.user == user_id) & (Notification.category == category))
+    query.execute()
+
+    flash('Notification(s) deleted successfully', 'success')
+    return redirect(url_for('notifications.index'))
